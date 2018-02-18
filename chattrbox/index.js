c1533b9f1063548fb9a07aabd9db9130524cc1f5
Peter Bødskov <pb@reeltrak.com>
2018-02-18 17:54:56 +0100

    starts work on chattrbox example

---
+++ chattrbox/index.js
@@ -0,0 +1,33 @@
+var http = require('http');
+var fs = require('fs');
+var extract = require('./extract');
+
+var handleError = function(err, res) {
+  res.writeHead(404);
+  res.end();
+};
+
+var showErrorPage = function(err, res) {
+  fs.readFile('app/error.html', function(err, data){
+    res.setHeader('Content-Type',
+  'text/html');
+    res.end(data);
+  });
+};
+
+var server = http.createServer(function(req, res){
+  console.log('Responding to a request.');
+  var filePath = extract(req.url);
+  fs.readFile(filePath, function(err, data){
+    if (err) {
+      //handleError(err, res);
+      showErrorPage(err, res);
+      return;
+    } else {
+      //set mime type
+      res.setHeader('Content-Type', 'text/html');
+      res.end(data);
+    }
+  });
+});
+server.listen(3000);
